735d0406e68f484e59705cf3f28bbad7231dcddd
Jonas Andersson <jonananas@gmail.com>
2014-03-31 13:27:17 +0200

    wordwrap added

---
+++ src/test/java/se/agileexchange/tdd/wordwrap/WordWrapper.java
@@ -0,0 +1,19 @@
+package se.agileexchange.tdd.wordwrap;
+
+class WordWrapper {
+
+	public String wrap(String text, int preferredLength) {
+		int wrapPoint = findWrapPoint(text, preferredLength);
+		if (wrapPoint <= 0)
+			return text;
+		return makeLine(text, wrapPoint) + wrap(text.substring(wrapPoint + 1), preferredLength);
+	}
+
+	private String makeLine(String text, int wrapPoint) {
+		return text.substring(0,wrapPoint) + "\n";
+	}
+
+	private int findWrapPoint(String text, int preferredLength) {
+		return text.indexOf(" ", preferredLength-1);
+	}
+}
---
+++ src/test/java/se/agileexchange/tdd/wordwrap2/WordWrapper.java
@@ -0,0 +1,20 @@
+package se.agileexchange.tdd.wordwrap2;
+
+class WordWrapper {
+
+	public String wrap(String text, int preferredLength) {
+		int wrapPoint = findWrapPoint(text, preferredLength);
+		if (wrapPoint <= 0)
+			return text;
+		return beforeWrapPoint(text, wrapPoint) + "\n" + wrap(text.substring(wrapPoint+1), preferredLength);
+	}
+
+	private int findWrapPoint(String text, int preferredLength) {
+		return text.indexOf(" ", preferredLength -1);
+	}
+
+	private String beforeWrapPoint(String text, int wrapPoint) {
+		return text.substring(0, wrapPoint);
+	}
+	
+}
