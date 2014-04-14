b0a71a235b7f754e6381f7526c437d5b1de808a5
Jonas Andersson <jonananas@gmail.com>
2014-04-14 09:21:21 +0200

    Fixed pom, can install now

--- src/test/java/se/jonananas/teaching/ddd/tacticalPatterns/Produkt.java
+++ src/test/java/se/jonananas/teaching/ddd/tacticalPatterns/Produkt.java
@@ -4,9 +4,9 @@ import java.io.Serializable;
 import java.util.UUID;
 
 
-@SuppressWarnings("serial")
-class Produkt implements Serializable {
+public class Produkt implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	private String name;
 	private UUID id;
 
