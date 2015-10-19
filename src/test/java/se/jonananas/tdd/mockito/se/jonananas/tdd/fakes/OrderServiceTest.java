d74cd57026adc14f9ba9bc410a1a12c8a369b6c4
Jonas Andersson <jonananas@gmail.com>
2015-10-19 20:47:51 +0200

    Updated to java 8 and utf-8 as property

--- src/test/java/se/jonananas/tdd/mockito/se/jonananas/tdd/fakes/OrderServiceTest.java
+++ src/test/java/se/jonananas/tdd/mockito/se/jonananas/tdd/fakes/OrderServiceTest.java
@@ -1,6 +1,6 @@
 package se.jonananas.tdd.mockito.se.jonananas.tdd.fakes;
 
-import org.fest.assertions.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 
