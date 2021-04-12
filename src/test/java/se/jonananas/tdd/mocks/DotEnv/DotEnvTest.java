08e1177cb3f8f1500540f2134c31cf6bba046c3c
Jonas Andersson <jonananas@gmail.com>
2021-04-12 20:57:24 +0200

    Add pro tip

--- src/test/java/se/jonananas/tdd/mocks/DotEnv/DotEnvTest.java
+++ src/test/java/se/jonananas/tdd/mocks/DotEnv/DotEnvTest.java
@@ -1,13 +1,15 @@
 package se.jonananas.tdd.mocks.DotEnv;
 
 /**
-* DotEnvFiles has a DotEnv read(Path path) method that returns Map<String, String>  
-* If path does not exist: Throws a java.nio.file.NoSuchFileException
-* For file "KEY1=VALUE1", dotenv.get("KEY1") should return VALUE1
-* For file "KEY1=VALUE1\nKEY2=VALUE2", dotenv.get("KEY2") should return VALUE2
+ * Pro tip:
+ * - Add org.assertj.core.api.Assertions to Favorites
+ * - Add org.mockito.Mockito to Favorites
+ * - Add org.mockito.ArgumentMatchers to Favorites
+ *
  */
 
 class DotEnvTest {
 
-
+	// mock here
+	
 }
