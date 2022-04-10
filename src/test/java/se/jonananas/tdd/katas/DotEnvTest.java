af8a4a8ce5ea8461f63c214c27f6006ed51d79a4
Jonas Andersson <jonananas@gmail.com>
2022-04-10 19:15:32 +0200

    Add import help

--- src/test/java/se/jonananas/tdd/katas/DotEnvTest.java
+++ src/test/java/se/jonananas/tdd/katas/DotEnvTest.java
@@ -8,9 +8,19 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * Pro tip:
  * - Add org.assertj.core.api.Assertions to Favorites
- * - Add org.mockito.Mockito to Favorites
  * - Add org.mockito.ArgumentMatchers to Favorites
+ * - Add org.mockito.Mockito to Favorites
  *
+ * Alternatively these imports can be helpful:
+ * import static org.assertj.core.api.Assertions.*;
+ * import static org.mockito.ArgumentMatchers.*;
+ * import static org.mockito.Mockito.*;
+ * 
+ * import org.junit.jupiter.api.extension.ExtendWith;
+ * import org.mockito.junit.jupiter.MockitoExtension;
+ * import org.mockito.ArgumentCaptor;
+ * import org.mockito.Captor;
+ * import org.mockito.Mock;
  */
 
 import org.junit.jupiter.api.Test;
